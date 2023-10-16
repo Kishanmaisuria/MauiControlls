914718f1f568be37a2fa684be5fdbdf8e2528c95
Kishanmaisuria <99621357+Kishanmaisuria@users.noreply.github.com>
2023-10-17 09:47:27 +1300

    Fixs1

---
+++ ContolsMaui/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using ContolsMaui.Pages;
+
+namespace ContolsMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CommonControls();
+        }
+    }
+}
---
+++ ContolsMaui/Pages/CommonControls.xaml.cs
@@ -0,0 +1,9 @@
+namespace ContolsMaui.Pages;
+
+public partial class CommonControls : ContentPage
+{
+	public CommonControls()
+	{
+		InitializeComponent();
+	}
+}
